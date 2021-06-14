c1e8dcb7db7c0d47ad7c64c5b83ac35cf64ed424
Auroraxpm <73517150+Auroraxpm@users.noreply.github.com>
2021-06-14 11:36:56 +0800

    clean commit

--- fancyss_arm384/config.json.js
+++ fancyss_arm384/config.json.js
@@ -2,7 +2,7 @@
 "build_date":"2021-06-010_12:17:16",
 "description":"科学上网",
 "home_url":"Module_shadowsocks.asp",
-"md5":"ab1e4aeedf5fffea0c44a0e82e83011f",
+"md5":"c50a555eddf126dcd69765fe39e79fdd",
 "name":"shadowsocks",
 "tar_url": "https://raw.githubusercontent.com/SURFBOARD-ONE/fancyss/master/fancyss_arm384/shadowsocks.tar.gz", 
 "title":"科学上网",
