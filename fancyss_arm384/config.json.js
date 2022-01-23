444d84e4f330ee22784131835db5dab19bcd0737
Auroraxpm <73517150+Auroraxpm@users.noreply.github.com>
2022-01-24 03:54:45 +0800

    clean commit

--- fancyss_arm384/config.json.js
+++ fancyss_arm384/config.json.js
@@ -1,10 +1,10 @@
 {
-"build_date":"2022-01-21_19:17:16",
+"build_date":"2022-01-24_19:17:16",
 "description":"科学上网",
 "home_url":"Module_shadowsocks.asp",
-"md5":"2d229ecb09206a6f1e6ee95d68220fa3",
+"md5":"f71a30db21df712a89d3f2720da2b16f",
 "name":"shadowsocks",
 "tar_url": "https://raw.githubusercontent.com/SURFBOARD-ONE/fancyss/master/fancyss_arm384/shadowsocks.tar.gz", 
 "title":"科学上网",
-"version":"2.1.7"
+"version":"2.1.8"
 }
--- fancyss_hnd/config.json.js
+++ fancyss_hnd/config.json.js
@@ -1,10 +1,10 @@
 {
-"build_date":"2022-01-21_12:17:16",
+"build_date":"2022-01-24_12:17:16",
 "description":"科学上网",
 "home_url":"Module_shadowsocks.asp",
-"md5":"8bf5ac6904e9cf28d61884329bc9648f",
+"md5":"a8b30a0a2856216f06b4532accb7080a",
 "name":"shadowsocks",
 "tar_url": "https://raw.githubusercontent.com/SURFBOARD-ONE/fancyss/master/fancyss_hnd/shadowsocks.tar.gz", 
 "title":"科学上网",
-"version":"2.0.7"
+"version":"2.0.8"
 }
